de5f20cdf6e1b1ae3abb65b31c75de23db8284a8
Angelenamorado777 <albertoenamorado123@gmail.com>
2025-12-15 19:01:21 -0600

    Commit - Examen MVVM

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Examen_MVVM;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Examen_MVVM;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
